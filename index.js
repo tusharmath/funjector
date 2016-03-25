5c068a501eaa083fc72eccf184c7ba4140b30ec1
Tushar Mathur <tusharmath@gmail.com>
2016-03-25 12:49:00 +0530

    feat(partial): add placeholder support

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const e = exports
 const toArray = (x, i) => Array.prototype.slice.call(x, i)
 const FUNJECTOR_KEY = Symbol.for('funjector')
+const PLACEHOLDER_KEY = Symbol.for('funjectorPlaceholder')
+
+e.skip = PLACEHOLDER_KEY
 
 e.partial = function (func) {
   const args = toArray(arguments, 1)
   const partialized = function () {
     const _args = toArray(arguments)
-    return (func.apply(this, args.concat(_args)))
+    const params = args
+      .map(x => x === PLACEHOLDER_KEY ? _args.shift() : x)
+      .concat(_args)
+    return (func.apply(this, params))
   }
   partialized[FUNJECTOR_KEY] = func
   return partialized
--- test.js
+++ test.js
@@ -1,10 +1,20 @@
 import test from 'ava'
-import { partial, call, callWith } from './index'
+import { partial, call, callWith, skip } from './index'
 
-test('partialize:args', t => {
+test('partial:args', t => {
   const a = partial((x, y) => x * y, 10)
   t.same(a(3), 30)
   t.same(a(4), 40)
+})
+
+test('partial:placeholders', t => {
+  const a = partial((a, b, c, d) => [a, b, c, d], 1, skip, skip, 4)
+  t.same(a(2, 3), [1, 2, 3, 4])
+})
+
+test('partial:placeholders:rest', t => {
+  const b = partial((a, b, c, d, e) => [a, b, c, d, e], 1, skip, skip, 4)
+  t.same(b(2, 3, 5), [1, 2, 3, 4, 5])
 })
 
 test('partial:this', t => {
